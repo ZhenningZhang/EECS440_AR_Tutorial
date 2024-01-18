c69544a90fc832ac16d62f7657782099c0def58a
Zhenning Zhang <zzn@gw-01.dmc.dc.umich.edu>
2024-01-17 20:46:49 -0500

    init

---
+++ Assets/ARObjectSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARObjectSpawner : MonoBehaviour
+{
+    [SerializeField] Transform cursor;
+    [SerializeField] GameObject gameobject_to_spawn;
+    [SerializeField] AudioClip spawn_sound_effect;
+
+    public void SpawnGameobjectAtCursor()
+    {
+        GameObject new_object = Instantiate(gameobject_to_spawn);
+        new_object.transform.SetPositionAndRotation(cursor.position, cursor.rotation);
+
+        AudioSource.PlayClipAtPoint(spawn_sound_effect, Camera.main.transform.position);
+    }
+}
